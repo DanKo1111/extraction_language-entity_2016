1c29cf0d87c7a9e4463160e082e8819e616a54b9
DanKo1111 <dankomail@mail.ru>
2016-05-31 00:26:03 +0300

    Update extract_stat.py

--- Py_files/extract_stat.py
+++ Py_files/extract_stat.py
@@ -1,24 +1,15 @@
+#Сбор статистики по обработанным токенам
 # -*- coding: utf-8 -*-
 import codecs, re, os, json
 
-##main_path = "d:\\daniil\\course3\\tokenized\\gramms_"
-###sources = ["blog", "ZhZ", "papers"]
-##sources = ["blog"]
-###sources = ["ZhZ"]
-###sources = ["papers"]
-##types = ["_adj_verb.json", "_adj_noun.json", "_verb_noun.json"]
-###[V, A, N]
 all_paths = []
-##for i in sources:
-##    for j in types:
-##        all_paths.append(main_path + i + j)
 
-def write_data (path, data):
+def write_data (path, data): # Запись в json
     json_data = json.dumps(data, ensure_ascii=False, indent=1)  
     json_file = codecs.open (path, 'w', 'utf-8')
     json_file.write (json_data)
     json_file.close()
-def read_data (path):
+def read_data (path): #Чтание Json
     data_file = codecs.open(path, 'r', 'utf-8')
     data = json.load(data_file)
     data_file.close()
@@ -33,7 +24,7 @@ def create_paths(directory): # С json файлами, содержащими т
                 all_paths.append(i[0] + "\\" + j)
         
 
-def lang_verb_stat(out_path, name_l, name_v):
+def lang_verb_stat(out_path, name_l, name_v): # Сбор статистики по прилагательным и глаголам
     langs_stat = {}
     verbs_stat = {}
     for f in all_paths:
@@ -58,7 +49,7 @@ def lang_verb_stat(out_path, name_l, name_v):
     write_data(out_path + name_l, langs_stat)
     write_data(out_path + name_v, verbs_stat)
 
-def lang_and_verb_stat(out_path, name):
+def lang_and_verb_stat(out_path, name): #Сбор статистики по сочетанию прилагательного и глагола
     lang_verb_stat = {}
     for f in all_paths:
         new_data = read_data(f)
@@ -93,41 +84,11 @@ def lang_and_verb_stat(out_path, name):
                     
     write_data(out_path + name, lang_verb_stat)
 
-def main (in_directory, out_directory):
+def main (in_directory, out_directory): #На вход подается путь к обрабатываемой директории, и директории, в которую будет производиться запись
     create_paths(in_directory)
     for i in all_paths:
         res = i.split("\\")
         name = res[-1][:-4]
         lang_verb_stat(out_directory, name + "_langs_stat.json", name + "_verbs_stat.json")
         lang_and_verb_stat(out_directory, name +  "_lang_and_verb_stat.json")
-
-##def verb_and_lang_stat(out_path, name):
-##    verb_lang_stat = {}
-##    for f in all_paths:
-##        new_data = read_data(f)
-##        for i in new_data:
-##            for j in i[2]:
-##                if j[0] not in verb_lang_stat:
-##                    verb_lang_stat.update({j[0]:{}})
-##    for f in all_paths:
-##        new_data = read_data(f)
-##        for i in new_data:
-##            loc_verbs = []
-##            for j in i[2]:
-##                loc_verbs.append(j[0])
-##            for j in loc_verbs:
-##                for k in i[3]:
-##                    if k[0] in verb_lang_stat[j]:
-##                        verb_lang_stat[j][k[0]] += 1
-##                    elif k[0][3:] + u"й" in verb_lang_stat[j]:
-##                        verb_lang_stat[j][k[0][3:] + u"й"] += 1
-##                    else:
-##                        if k[0][:3] == u"по-":
-##                            verb_lang_stat[j].update({k[0][3:] + u"й":1})
-##                        else:
-##                            verb_lang_stat[j].update({k[0]:1})
-##                
-##    write_data(out_path + name, verb_lang_stat)
-
-
-##verb_and_lang_stat("d:\\daniil\\course3\\stat\\", sources[0] + "_" + "verb_and_lang_stat.json")
+main("input", "output")
