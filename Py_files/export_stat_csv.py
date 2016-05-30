a6b7fd65e16ab5ff9ace43450c0841ceb546f1cb
DanKo1111 <dankomail@mail.ru>
2016-05-31 00:13:22 +0300

    Update export_stat_csv.py

--- Py_files/export_stat_csv.py
+++ Py_files/export_stat_csv.py
@@ -1,18 +1,19 @@
+#Запись статистики из json в формат csv
 # -*- coding: utf-8 -*-
 import codecs, os, json
 
-def write_data (path, data):
+def write_data (path, data): #Запись в json
     json_data = json.dumps(data, ensure_ascii=False, indent=1)  
     json_file = codecs.open (path, 'w', 'utf-8')
     json_file.write (json_data)
     json_file.close()
-def read_data (path):
+def read_data (path): #Чтение json
     data_file = codecs.open(path, 'r', 'utf-8')
     data = json.load(data_file)
     data_file.close()
     return data
 
-def rewrite_csv(in_path, out_path):
+def rewrite_csv(in_path, out_path): #Запись данных из json файла в csv
     #print in_path, out_path
     data = read_data(in_path)
     text = u""
@@ -36,7 +37,7 @@ def rewrite_csv(in_path, out_path):
     a = codecs.open(out_path, "w", "utf-8")
     a.write(text)
     a.close()
-def main(path, out):
+def main(path, out): #На фход подается путь к директории с обрабатываемыми файлами и путь к директории, в которую следует записать результат
     for i in os.walk(path):
         res = i[2]
         if res:
